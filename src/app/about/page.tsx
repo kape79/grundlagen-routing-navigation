184c59bfbca2c9ae88f8f6eb07645eb98b022561
Kai Petersen <kaipetersen@MacStudovonKai2.wi.fh-flensburg.de>
2026-04-08 10:18:06 +0200

    Implemented Metadata examples

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,7 @@
 export default function About() {
     return <h1>About me</h1>;
 }
+
+export const metadata = {
+    title: "About page"
+}
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,19 @@
+import { Metadata } from "next";
+
+type Props = {
+    params: Promise<{ productId: string}>; // params is a Promis that resolves to productId of type string. 
+};
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+    const id = (await params).productId;
+    return {
+        title: `Product ${id}`,
+    }
+}
+
 export default async function ProductDetails({
     params,
-}: {
-    params: Promise<{productId: string}>;
-}) {
+}: Props) {
     const productId = (await params).productId;
     return <h1>Here are the product details {productId}</h1>;
 }
