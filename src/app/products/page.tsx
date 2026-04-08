bc69c847d48150bb90dc2ce4b93b4047d7f9f022
Kai Petersen <kaipetersen@MacStudovonKai2.wi.fh-flensburg.de>
2026-04-08 15:58:23 +0200

    Added links to individual products

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -5,8 +5,14 @@ export default function ProductList() {
     return <>
         <Link href="/">Home</Link>
         <h1>Product list</h1>
-        <h2>Product 1</h2>
-        <h2>Product 2</h2>
-        <h2>Product 3</h2>
+        <h2>
+            <Link href="/products/1">Product 1</Link>
+        </h2>
+        <h2>
+            <Link href="/products/2">Product 2</Link>
+        </h2>
+        <h2>
+            <Link href="/products/3">Product 3</Link>
+        </h2>
     </>
 }
