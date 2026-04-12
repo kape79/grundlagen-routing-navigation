c3d3eec1cc185df069ccaebfc211ac76c14e253c
Kai Petersen <kaipetersen@MacStudovonKai2.wi.fh-flensburg.de>
2026-04-12 13:17:20 +0200

    Added loading.tsx to blog to demonstrate loading pages

---
+++ src/app/blog/loading.tsx
@@ -0,0 +1,11 @@
+import { Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: {
+        absolute: "Blog",
+    }
+}
+
+export default function Loading() {
+    return <h1>Loading</h1>
+}
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -6,6 +6,13 @@ export const metadata: Metadata = {
     },
 };
 
-export default function Blog() {
+export default async function Blog() {
+    await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Intentional delay");
+        }, 2000)
+    }
+
+    )
     return <h1>My blog</h1>;
 }
